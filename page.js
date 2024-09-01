8f7125fd7cc8b077ae278da0e90cf78cdd9161f3
kitamura-mia <sakoda0320@gmail.com>
2024-09-02 02:36:56 +0900

    Update page.js

--- page.js
+++ page.js
@@ -8,3 +8,19 @@ $(function() {
       $("body").toggleClass("active");
   });
   });
+
+document.addEventListener("mousemove", (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  gsap.to(".circle", {
+    x: mouseX,
+    y: mouseY,
+    stagger: -0.1,
+  });
+
+  gsap.set(".cursor", {
+    x: mouseX,
+    y: mouseY,
+  });
+});
