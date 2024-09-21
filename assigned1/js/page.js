9e9b8051c4a98243978acb3fc409c827de9b33b6
kitamura-mia <sakoda0320@gmail.com>
2024-09-21 22:24:18 +0900

    Add files via upload

---
+++ assigned1/js/page.js
@@ -0,0 +1,28 @@
+$(function() {
+    $('#gnav a').on('click', function(event) {
+        $('#btn_open').prop('checked',false);
+    });
+  
+    $("#gnav a,.btn_menu").click(function () {
+      $(this).toggleClass("active");
+      $("body").toggleClass("active");
+  });
+});
+  
+
+
+document.addEventListener("mousemove", (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  gsap.to(".circle", {
+    x: mouseX,
+    y: mouseY,
+    stagger: -0.1,
+  });
+
+  gsap.set(".cursor", {
+    x: mouseX,
+    y: mouseY,
+  });
+});
